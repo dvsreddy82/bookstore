bbabf5604dd78a5025d0b662770c164d4abe379a
Ravikiran Reddy K <kravikiran@ssdi.sharp.co.in>
2016-12-16 14:34:57 +0530

    Template language and static files

--- store/views.py
+++ store/views.py
@@ -8,11 +8,12 @@ def store(request):
     count = Book.objects.all().count()
     context = {
         'count':count,
+        'page':'welcome to books',
     }
     request.session['location'] = "unknown"
     if request.user.is_authenticated():
         request.session['location'] = "Earth"
-    return render(request,'store.html',context)
+    return render(request,'base.html',context)
 
 
 def count(request):
