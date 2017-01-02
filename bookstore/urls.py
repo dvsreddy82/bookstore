d69300246e22ddaf629cb0b5588c4a3e6f45d4a5
Ravikiran Reddy K <kravikiran@ssdi.sharp.co.in>
2017-01-02 15:54:06 +0530

    File upload

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from registration.backends.default.urls import *
 from social.apps.django_app.urls import *
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     # Examples:
     # url(r'^$', 'bookstore.views.home', name='home'),
@@ -11,4 +13,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url('',include('social.apps.django_app.urls',namespace='social')),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
